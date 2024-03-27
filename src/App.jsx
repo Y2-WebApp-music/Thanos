9f0d80a56467f0c272b0f9365b54bc248eff863b
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-28 05:26:37 +0700

    🥦 Google font

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import react from 'react'
 import './App.css'
 
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Chat from './pages/Chat'
 
 function App() {
 
