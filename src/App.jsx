d03d213580a4a5b7d69bc7d27c521c7a1bd0a75d
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-28 05:13:41 +0700

    🫡~~~~

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import react from 'react'
 import './App.css'
 
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Chat from './pages/Chat'
+
 function App() {
 
   return (
     <>
+      <Home/>
     </>
   )
 }
