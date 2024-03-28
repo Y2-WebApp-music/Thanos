7260ea2cc31233a75d87fb1d3ce0765cd770b1f3
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-03-28 21:33:31 +0700

    🤧npm Firebase

--- src/components/Chat/ChatContent.jsx
+++ src/components/Chat/ChatContent.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import './ChatContent.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function ChatContent() {
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        autoExpand();
+    }, []);
+
+    const autoExpand = () => {
+        const textarea = textareaRef.current;
+        textarea.style.height = '1.5em';
+        textarea.style.height = Math.min(textarea.scrollHeight, 170) + 'px';
+    };
+
+    const handleChange = () => {
+        autoExpand();
+    };
     return(
         <>
             <div className="ChatContent-container">
@@ -12,11 +27,18 @@ function ChatContent() {
                 </div>
                 <div className="bottom-chat-input">
                     <div className="input-Container">
-                        <textarea name="promptInput" placeholder="คุยกับ 1MAN&3GUY............." id="" ></textarea>
+                        <textarea ref={textareaRef} name="promptInput" placeholder="คุยกับ 1MAN&3GUY............." id=""  onChange={handleChange}></textarea>
                         <FontAwesomeIcon icon={faPaperPlane} size="2xl" id="faPaperPlane"/>
                     </div>
                 </div>
             </div>
+        </>
+    )
+}
+
+function userChat() {
+    return(
+        <>
         </>
     )
 }
