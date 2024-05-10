709d2c1516aef8d87f781dd7ef36f90bb4fdca28
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-05-10 21:18:56 +0700

    💄 Update cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import react, {useState, useEffect, useContext} from 'react'
+import {useContext} from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
-import Cookies from 'universal-cookie'
-const cookies = new Cookies()
-import { auth,db } from '/src/DB/firebase-config.js'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -12,10 +9,7 @@ import ChatPage from './pages/ChatPage'
 import { AuthContext } from './DB/Auth';
 
 function App() {
-  // const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
   const {currentUser} = useContext(AuthContext)
-
-  // console.log(currentUser)
 
   const ProtectedRoute = ({children}) => {
     if(!currentUser){
