0464390cf289b77269c073c1475daf46d584bc11
nut-somwang <82942709+guynut@users.noreply.github.com>
2024-05-15 22:49:04 +0700

    💄 DB Function

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express, { json } from 'express'
+import cors from 'cors'
+import { readChat, updateChatName, deleteChat, readMessage, addMessage, createChat, createMessages } from './mongo';
 const tf = require('@tensorflow/tfjs-node'); // Or use 'torch' for PyTorch models
 
 const app = express();
@@ -13,11 +13,66 @@ async function loadModel() {
 }
 loadModel();
 
+// ============
+//  Model POST
+// ============
 app.post('/predict', async (req, res) => {
     const inputs = tf.tensor(req.body.inputs);
     const predictions = model.predict(inputs);
     res.json(predictions.arraySync());
 });
+
+// ======================
+//  Chat Room GET and POST
+// ======================
+app.post('/addChatRoom', (req, res)=>{
+    const uid = req.query.uid;
+    const document = req.body;
+    createChat(uid,document)
+    .catch(err => res.json(err))
+})
+app.get('/chatroom', async (req, res) => {
+    const uid = req.query.uid;
+    readChat(uid)
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
+})
+app.post('/updateChatName', async (req, res) => {
+    const uid = req.query.uid;
+    const chatId = req.query.chatId;
+    const update = req.query.uid;
+    updateChatName(uid, chatId, update)
+    .catch(err => res.json(err))
+})
+app.post('/deletePlan', (req, res)=>{
+    const uid = req.query.uid;
+    const chatId = req.query.chatId;
+    deleteChat(uid, chatId)
+    .catch(err => res.json(err))
+})
+// ======================
+//  Messages GET and POST
+// ======================
+app.post('/newMessageRoom', (req, res)=>{
+    const chatId = req.query.chatId;
+    const document = req.body;
+    createMessages(chatId, document)
+    .catch(err => res.json(err))
+})
+app.get('/messages', async (req, res) => {
+    const chatId = req.query.chatId;
+    readMessage(chatId)
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
+})
+app.post('/addMessages', (req, res)=>{
+    const chatId = req.query.chatId;
+    const document = req.body;
+    addMessage(chatId, document)
+    .catch(err => res.json(err))
+})
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/mongo.js
+++ server/mongo.js
@@ -5,14 +5,117 @@ const url = "mongodb+srv://Guynut:<password>@clustercapstone.ssajl5d.mongodb.net
 const client = new MongoClient(url);
 
 let db;
-let collection;
+let collectionChat;
+let collectionMessages;
 
 async function connect() {
     await client.connect();
     console.log('')
     console.log("==> Connected to MongoDB");
-    db = client.db("Plan");
-    collection = db.collection("Plans");
+    db = client.db("Capstone");
+}
+
+// ======================
+//  Chat Room GET and POST
+// ======================
+export async function createChat(uid, document) {
+    try {
+        await connect();
+        collectionChat = db.collection("ChatRoom");
+        const result = await collectionChat.insertOne({ uid, ...document });
+        console.log(`createChat _id: ${result.insertedId}`);
+        close();
+    } catch (error) {
+        console.error("Error createChat :", error);
+        throw error;
+    }
+}
+export async function readChat(uid) {
+    try {
+        await connect();
+        collectionChat = db.collection("ChatRoom");
+        const documents = await collectionChat.find({ uid }).toArray();
+        console.log(" >> readChat <<");
+        close()
+        return documents;
+    } catch (error) {
+        console.error("Error readChat documents:", error);
+        throw error;
+    }
+}
+export async function updateChatName(uid, chatId, update) {
+    try {
+        await connect();
+        collectionChat = db.collection("ChatRoom");
+        const result = await collectionChat.updateOne(
+            { _id: new ObjectId(chatId), uid },
+            { $set: update }
+        );
+        console.log(`updateChatName _id: ${chatId}`);
+        close();
+    } catch (error) {
+        console.error("Error updateDocument :", error);
+        throw error;
+    }
+}
+
+export async function deleteChat(uid, chatId) {
+    try {
+        await connect();
+        collectionChat = db.collection("ChatRoom");
+        console.log("uid : ",uid)
+        console.log("id : ",documentId)
+        const result = await collectionChat.deleteOne({ _id: new ObjectId(chatId), uid });
+        console.log(`deleteChat _id: ${documentId}`);
+        close();
+    } catch (error) {
+        console.error("Error deleteDocument :", error);
+        throw error;
+    }
+}
+
+// ======================
+//  Messages GET and POST
+// ======================
+export async function createMessages(chatId, document) {
+    try {
+        await connect();
+        collectionMessages = db.collection("Messages");
+        const result = await collectionMessages.insertOne({ chatId, ...document });
+        console.log(`createMessages _id: ${result.insertedId}`);
+        close();
+    } catch (error) {
+        console.error("Error createMessages :", error);
+        throw error;
+    }
+}
+export async function readMessage( chatId ) {
+    try {
+        await connect();
+        collectionMessages = db.collection("Messages");
+        const documents = await collectionMessages.find({ chatId }).toArray();
+        console.log(" >> readMessage <<");
+        close()
+        return documents;
+    } catch (error) {
+        console.error("Error readMessage documents:", error);
+        throw error;
+    }
+}
+export async function addMessage(chatId, document) {
+    try {
+        await connect();
+        collectionMessages = db.collection("Messages");
+        const result = await collectionMessages.updateOne(
+            { _id: new ObjectId(chatId) },
+            { $set: document }
+        );
+        console.log(`addMessage _id: ${result.insertedId}`);
+        close();
+    } catch (error) {
+        console.error("Error addMessage :", error);
+        throw error;
+    }
 }
 
 async function close() {
